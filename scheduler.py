f3a300ea1d4f4249e3437a67ac12994b02b211eb
Lauren M <Lmurphy@gmx.com>
2022-06-22 12:57:11 -0700

    Moved scheduler out to own file

---
+++ scheduler.py
@@ -0,0 +1,20 @@
+import schedule
+from schedule import every, repeat
+import time
+
+
+@repeat(every(1).second)
+def check_for_overdue_tasks():
+    #from tasks.models import Task
+    # my_data = Task.objects.all().values()
+    # get all users and tasks from db
+    # for each user, 
+    # for each task, 
+    # if a task's due date is greater than the current date, 
+    # send message via twillo
+    print(f"Checked for overdue tasks.")
+    #print(my_data)
+
+while True:
+     schedule.run_pending()
+     time.sleep(1)
