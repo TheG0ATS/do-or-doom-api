b86f8ced928f63f0d16fb5ae0d74ba4682dc0828
Lauren M <Lmurphy@gmx.com>
2022-06-22 10:37:43 -0700

    Added comments

--- accounts/models.py
+++ accounts/models.py
@@ -3,7 +3,8 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     pass
-    # add additional fields in here
-
+    # Include default values for both!!
+    # User name? (The name the user is addressed by in texts/messages)
+    # User accountability partner phone number?
     def __str__(self):
         return self.username
