83719391c5c60099610ad6124c6a4624d8534927
Lauren M <Lmurphy@gmx.com>
2022-06-21 19:44:39 -0700

    Added schedule code (still WIP)

--- tasks/apps.py
+++ tasks/apps.py
@@ -1,5 +1,29 @@
 from django.apps import AppConfig
+import schedule
+import time
+from tasks.models import Task
 
+app_ready = False
 
 class TasksConfig(AppConfig):
     name = 'tasks'
+    def ready(self):
+        app_ready=True
+
+
+def check_for_overdue_tasks():
+    my_data = Task.objects.all().values()
+
+    # get all users and tasks from db
+    # for each user, 
+    # for each task, 
+    # if a task's due date is greater than the current date, 
+    # send message via twillo
+    print("Checked for overdue tasks.")
+    print(my_data)
+
+schedule.every(1).seconds.do(check_for_overdue_tasks)
+
+while app_ready:
+    schedule.run_pending()
+    time.sleep(1)
